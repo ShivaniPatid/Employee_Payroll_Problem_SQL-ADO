eba8ac7aec9725753c8e1fd67fcb0a1e96084a74
ShivaniPatid <shivanipatidar712@gmail.com>
2022-10-13 19:26:16 +0530

    [Shivani] Retrieve the Employee Payroll from the Database

---
+++ EmployeePayrollProblem/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace EmployeePayrollProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom to employee payroll");
+
+            EmployeeModel model = new EmployeeModel();
+            EmployeeRepo repo = new EmployeeRepo();
+
+            model.EmployeeName = "Roanak";
+            model.PhoneNumber = 9999999999;
+            model.Address = "Pune";
+            model.Department = "HR";
+            model.Gender = 'M';
+            model.BasicPay = 22000;
+            model.Deduction = 1500;
+            model.TaxablePay = 200;
+            model.Tax = 300;
+            model.NetPay = 2500;
+            model.City = "Pune";
+            model.Country = "India";
+
+            //repo.AddEmployee(model);
+            repo.GetAllEmployee();
+            
+        }
+    }
+}
